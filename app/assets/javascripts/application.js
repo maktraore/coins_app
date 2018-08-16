4e393705f8b975f3bbf3f78b83328ad86d7994bd
Mamadou Traore <maktraore2000@gmail.com>
2018-08-16 00:58:56 -0400

    fix some front end

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,14 +18,23 @@
 //= require_tree .
 
 $(document).ready(function() {
-    var table = $('#receivers').DataTable({
+    var t = $('#receivers').DataTable( {
       dom: "<'row'<'col-sm-3'l><'col-sm-3'f><'col-sm-6'p>>" +
       "<'row'<'col-sm-12'tr>>" +
       "<'row'<'col-sm-5'i><'col-sm-7'p>>",
       "lengthMenu": [[10, 20, 30, -1], [10, 20, 30, "All"]],
+        "order": [[ 1, 'asc' ]],
+        
     } );
+ 
+    t.on( 'order.dt search.dt', function () {
+        t.column(0, {search:'applied', order:'applied'}).nodes().each( function (cell, i) {
+            cell.innerHTML = i+1;
+        } );
+    } ).draw();
 
-    var table = $('#admins').DataTable({
+
+    var t = $('#admins').DataTable({
       dom: "<'row'<'col-sm-3'l><'col-sm-3'f><'col-sm-6'p>>" +
       "<'row'<'col-sm-12'tr>>" +
       "<'row'<'col-sm-5'i><'col-sm-7'p>>",
--- app/controllers/givers_controller.rb
+++ app/controllers/givers_controller.rb
@@ -2,7 +2,7 @@ class GiversController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   # attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   def index
-    @receivers = Receiver.all.order('updated_at DESC')
+    @receivers = Receiver.all.order('created_at ASC')
     @user= User.find_by(id: params[:id])
 
   end
@@ -13,6 +13,7 @@ class GiversController < ApplicationController
 
   def create
     @receiver = Receiver.create(giver_first_name: params[:giver_first_name], giver_last_name: params[:giver_last_name], first_name: params[:first_name], last_name: params[:last_name],email: params[:email], department: params[:department], location: params[:location], race: params[:race], coins:params[:coins])
+    flash[:success] = "The receiver information has been saved"
            UserMailer.welcome_email(@receiver).deliver!
   end
 

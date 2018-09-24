39c811c3c5470aefb6bca6991203bf2f21137b08
Mamadou Traore <maktraore2000@gmail.com>
2018-09-24 11:47:08 -0400

    little changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-2.times do 
-  Receiver.create(
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    email:  Faker::Internet.email,
-    race: ['R','A','C','E'].sample,
-    department: ["CAEI", "Math Department", "CS department"].sample,
-    location: ["MT", "YT", "DF", "BX"].sample,
-    giver_first_name: Faker::Name.first_name,
-    giver_last_name: Faker::Name.last_name,
-    coins: rand(1..9)
-    )
-end
+# Receiver.all.delete_all
+# 20.times do 
+#   Receiver.create(
+#     first_name: Faker::Name.first_name,
+#     last_name: Faker::Name.last_name,
+#     email:  Faker::Internet.email,
+#     race: ['Responsive','Accurate','Courteous','Efficient'].sample,
+#     department: ["CAEI", "Administrative Services", "Compliance Office", "Office for Teaching Excellence and Engaged Learning","President's Office","School of Liberal Arts"].sample,
+#     location: ["Manhattan", "Yorktown", "Dobbs Ferry", "Bronx", "Tarrytown"].sample,
+#     giver_first_name: Faker::Name.first_name,
+#     giver_last_name: Faker::Name.last_name,
+#     coins: rand(1..9)
+#     )
+# end
 

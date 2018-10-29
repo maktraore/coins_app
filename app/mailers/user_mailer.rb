465e7d861b1ab2b25d8d9dbdab507fa75fff41f1
Mamadou Traore <maktraore2000@gmail.com>
2018-10-29 06:36:02 -0400

    added service-excellence link and delete recipients by 10 and 20

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,7 @@ class UserMailer < ApplicationMailer
   default from: 'gmtesting17@gmail.com'#'notifications@learningcenters.com'
   def welcome_email(user)
     @receiver = user 
-    @url = 'http://www.google.com'
+    @url = 'https://www.mercy.edu/service-excellence'
     mail(to: @receiver.email, subject: 'Cogratulation!')
   end
 end

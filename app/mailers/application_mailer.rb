0574bd3b4cc80a08bacf677826e944adae917984
Mamadou Traore <maktraore2000@gmail.com>
2019-01-16 21:21:57 -0500

    some more3 changes to email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: 'gmtesting17@gmail.com'
-  default from: 'coinsapp@mercy.edu'
+  default from: 'excellence.token@mercy.edu'
   layout 'mailer'
 end
